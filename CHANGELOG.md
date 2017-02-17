3d2161f99d52778bb43092f74e1429948d0392f3
Azat Khadiev <akhadiev@parallels.com>
2017-02-17 11:53:48 +0300

    Version bumped to 0.2.1

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.2.1 (Feb 17, 2017)
+  - Set default version of Kibana to 5.2.1.
+
+## 0.2.0 (Feb 14, 2017)
+  - Initial cookbook. See README.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'akhadiev@parallels.com'
 license          'Apache 2.0'
 description      'Installs/Configures kibana 5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 issues_url       'https://github.com/anuriq/chef-kibana5/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/anuriq/chef-kibana5' if respond_to?(:source_url)
 
