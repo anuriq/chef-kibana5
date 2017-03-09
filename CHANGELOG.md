d7903ed4c7a2bd7fbc0b90dfbe660d6c9a4965f9
Azat Khadiev <akhadiev@parallels.com>
2017-03-09 14:35:36 +0300

    Version bumped to 0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
-## 0.2.1 (Feb 17, 2017)
+## 0.2.2 (March 9, 2017)
   - Set default version of Kibana to 5.2.1.
 
-## 0.2.0 (Feb 14, 2017)
+## 0.2.1 (February 17, 2017)
+  - Set default version of Kibana to 5.2.1.
+
+## 0.2.0 (February 14, 2017)
   - Initial cookbook. See README.
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,21 @@
+#
+# Cookbook:: kibana5
+# Attribute:: default
+#
+# Copyright:: 2017, Parallels International GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 default['kibana5']['config']['server.port'] = 5601
 default['kibana5']['config']['server.host'] = 'localhost'
 default['kibana5']['config']['elasticsearch.url'] = 'http://localhost:9200'
