b611dd7ef13b2d6efdf9ac606611a7da482eb4e5
Azat Khadiev <akhadiev@parallels.com>
2017-07-12 11:44:26 +0300

    fix by suggestions from new foodcritic

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'kibana5'
 maintainer       'Azat Khadiev'
 maintainer_email 'anuriq@gmail.com'
-license          'Apache 2.0'
+license          'Apache-2.0'
 description      'Installs/Configures kibana 5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.1'
@@ -15,4 +15,4 @@ supports 'ubuntu', '>= 16.04'
 depends 'ark'
 depends 'systemd'
 
-chef_version '>= 12' if respond_to?(:chef_version)
+chef_version '>= 12.1' if respond_to?(:chef_version)
