07ebaf9f4c7d20634858725ea06de0d1df65309c
Christophe Courtaut <christophe.courtaut@gmail.com>
2018-04-27 14:50:34 +0200

    Bumps to latest systemd cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'anuriq@gmail.com'
 license          'Apache-2.0'
 description      'Installs/Configures kibana 5'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.5.0'
+version          '1.5.1'
 issues_url       'https://github.com/anuriq/chef-kibana5/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/anuriq/chef-kibana5' if respond_to?(:source_url)
 
@@ -13,6 +13,6 @@ supports 'redhat', '>= 7.0'
 supports 'ubuntu', '>= 16.04'
 
 depends 'ark'
-depends 'systemd', '~> 2.0'
+depends 'systemd', '>= 3'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
