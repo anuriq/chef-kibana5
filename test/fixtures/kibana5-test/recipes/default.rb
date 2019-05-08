9a381b6bed84f3a8cb730638e965fc276e875b08
Azat Khadiev <a.hadiev@corp.mail.ru>
2019-05-08 12:00:44 +0300

    change to OpenJDK to fix testing cookbook (#48)

--- test/fixtures/kibana5-test/recipes/default.rb
+++ test/fixtures/kibana5-test/recipes/default.rb
@@ -1,7 +1,6 @@
 # Install Java
-node.default['java']['oracle']['accept_oracle_download_terms'] = true
 node.default['java']['jdk_version'] = 8
-node.default['java']['install_flavor'] = 'oracle'
+node.default['java']['install_flavor'] = 'openjdk'
 include_recipe 'java'
 
 # Install Elasticsearch for Kibana instance
